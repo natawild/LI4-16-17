1e93aaec21f40e2f3468ed19ebb88fa670253953
Ricardo Lopes <lopesricardo75@gmail.com>
2017-06-06 19:54:20 +0100

    user

--- code/TasteAdvisor/TasteAdvisor/User.cs
+++ code/TasteAdvisor/TasteAdvisor/User.cs
@@ -37,6 +37,29 @@ namespace TasteAdvisor
             set { _email = value; }
         }
 
+        List<string> _ingredientesBloqueados;
+
+        public List<string> IngredientesBloqueados
+        {
+            get { return _ingredientesBloqueados; }
+            set { _ingredientesBloqueados = value; }
+        }
+
+        List<Prato> _pratosBloqueados;
+
+        public List<Prato> PratosBloqueados
+        {
+            get { return _pratosBloqueados; }
+            set { _pratosBloqueados = value; }
+        }
+
+        List<Restaurante> _restauranteBloqueados;
+
+        public List<Restaurante> RestaurantesBloqueados
+        {
+            get { return _restauranteBloqueados; }
+            set { _restauranteBloqueados = value; }
+        }
         #endregion
 
         #region Methods
