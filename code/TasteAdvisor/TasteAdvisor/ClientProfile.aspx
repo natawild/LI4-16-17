10fd1f4146e73c5705c7b7e22742771bae2b0205
Ricardo Lopes <lopesricardo75@gmail.com>
2017-06-09 23:28:37 +0100

    client page

--- code/TasteAdvisor/TasteAdvisor/ClientProfile.aspx
+++ code/TasteAdvisor/TasteAdvisor/ClientProfile.aspx
@@ -9,54 +9,58 @@
         </div>
         <div id="mainrow" class="row">
             <%--Inicio TreeView Lateral--%>
-            <div class="col-xs-4 col-xs-offset-1 col-sm-4 col-sm-offset-1 col-md-4 col-md-offset-1 col-lg-4 col-lg-offset-1">
+            <div class="col-xs-2 col-xs-offset-1 col-sm-2 col-sm-offset-1 col-md-2 col-md-offset-1 col-lg-2 col-lg-offset-1">
                 <p></p>
-                <asp:TreeView ID="menuTreeView" runat="server" OnLoad="menuTreeView_Load" />
+                <asp:TreeView ID="menuTreeView" runat="server" OnLoad="menuTreeView_Load" OnSelectedNodeChanged="menuTreeView_SelectedNodeChanged" />
             </div>
             <%--Fim TreeView Lateral--%>
-            <div id="maincol" class="col-xs-12 col-xs-offset-1 col-sm-12 col-sm-offset-1 col-md-12 col-md-offset-1 col-lg-12 col-lg-offset-1">
+            <div id="maincol" class="col-xs-3 col-xs-offset-0 col-sm-3 col-sm-offset-0 col-md-5 col-md-offset-0 col-lg-5 col-lg-offset-0">
                 <%--Inicio do Menu Favoritos--%>
                     <%--Restaurantes favoritos--%>
-                    <div class="row">
+                    <asp:Panel ID="restaurantesFavoritosPanel" runat="server">
                         <div class="col-xs-6 col-xs-offset-1 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1">
+                            <p>Restaurantes favoritos:</p>
                             <asp:ListView runat="server" ID="listviewrestaurantesfavoritos">
                                 <ItemTemplate>
                                 </ItemTemplate>
                             </asp:ListView>
                         </div>
-                    </div>
+                    </asp:Panel>
                     <%--Pratos favoritos--%>
-                    <div class="row">
+                    <asp:Panel ID="pratosFavoritosPanel" runat="server">
                         <div class="col-xs-6 col-xs-offset-1 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1">
+                            <p>Pratos favoritos:</p>
                             <asp:ListView runat="server" ID="listviewpratosfavoritos">
                                 <ItemTemplate>
                                 </ItemTemplate>
                             </asp:ListView>
                         </div>
-                    </div>
+                    </asp:Panel>
                 <%--Fim do Menu Favoritos--%>
                 <%--Inicio do Menu Nao favoritos--%>
                     <%--Restaurantes não favoritos--%>
-                    <div class="row">
+                    <asp:Panel ID="restaurantesNaoFavoritosPanel" runat="server">
                         <div class="col-xs-6 col-xs-offset-1 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1">
+                            <p>Restaurantes não favoritos:</p>
                             <asp:ListView runat="server" ID="listviewrestaurantesnaofavoritos">
                                 <ItemTemplate>
                                 </ItemTemplate>
                             </asp:ListView>
                         </div>
-                    </div>
+                    </asp:Panel>
                     <%--Pratos não favoritos--%>
-                    <div class="row">
+                    <asp:Panel ID="pratosNaoFavoritosPanel" runat="server">
                         <div class="col-xs-6 col-xs-offset-1 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1">
+                            <p>Pratos não favoritos:</p>
                             <asp:ListView runat="server" ID="listviewpratosnaofavoritos">
                                 <ItemTemplate>
                                 </ItemTemplate>
                             </asp:ListView>
                         </div>
-                    </div>
+                    </asp:Panel>
                 <%--Fim do Menu Nao favoritos --%>
                 <%--Inicio do Menu Outras preferencias--%>
-                <div class="row">
+                <asp:Panel ID="outrasPreferenciasPanel" runat="server">
                     <div class="col-xs-6 col-xs-offset-1 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1">
                         <asp:Label runat="server" Text="Outras Preferencias" Font-Size="Medium" />
                         <asp:Panel ID="outrasPreferencias" runat="server" BorderWidth="1" BorderColor="Black" Width="800">
@@ -111,33 +115,35 @@
                             </div>
                         </asp:Panel>
                     </div>
-                </div>
+                </asp:Panel>
                 <%--Fim do Menu outras preferencias--%>
 
                 <%--Inicio do Menu meus comentarios--%>
-                <div class="row">
+                <asp:Panel ID="meusComentariosPanel" runat="server">
                     <div class="col-xs-6 col-xs-offset-1 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1">
+                        <p>Meus comentários:</p>
                         <asp:ListView runat="server" ID="listviewmenucomentarios">
                             <ItemTemplate>
                             </ItemTemplate>
                         </asp:ListView>
                     </div>
-                </div>
+                </asp:Panel>
                 <%--Fim do Menu meus comentarios--%>
 
                 <%--Inicio do Menu Minhas fotografias--%>
-                <div class="row">
+                <asp:Panel ID="minhasFotografiasPanel" runat="server">
                     <div class="col-xs-6 col-xs-offset-1 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1">
+                        <p>Minhas fotografias:</p>
                         <asp:ListView runat="server" ID="listviewminhasfotografias">
                             <ItemTemplate>
                             </ItemTemplate>
                         </asp:ListView>
                     </div>
-                </div>
+                </asp:Panel>
                 <%--Fim do Menu Minhas fotografias--%>
 
                 <%--Inicio Menu Editar Perfil--%>
-                <div class="row">
+                <asp:Panel ID="editarPerfilMainPanel" runat="server">
                     <div class="col-xs-8 col-xs-offset-5 col-sm-8 col-sm-offset-5 col-md-8 col-md-offset-5 col-lg- col-lg-offset-5">
                         <asp:Label runat="server" Text="Editar Perfil" />
                         <!--panel-->
@@ -148,27 +154,27 @@
                                     <div class="col-md-6">Nome</div>
                                     <div class="col-md-6">col2</div>
                                     <div class="col-md-6">
-                                        <asp:LinkButton runat="server" ID="LinkButton1" runat="server">Editar</asp:LinkButton>
+                                        <asp:LinkButton runat="server" ID="LinkButton1">Editar</asp:LinkButton>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-6">Contacto</div>
                                     <div class="col-md-6">col2</div>
                                     <div class="col-md-6">
-                                        <asp:LinkButton runat="server" ID="LinkButton2" runat="server">Editar</asp:LinkButton>
+                                        <asp:LinkButton runat="server" ID="LinkButton2">Editar</asp:LinkButton>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-6">Palavra-Passe</div>
                                     <div class="col-md-6">col2</div>
                                     <div class="col-md-6">
-                                        <asp:LinkButton runat="server" ID="LinkButton3" runat="server">Editar</asp:LinkButton>
+                                        <asp:LinkButton runat="server" ID="LinkButton3">Editar</asp:LinkButton>
                                     </div>
                                 </div>
                             </div>
                         </asp:Panel>
                     </div>
-                </div>
+                </asp:Panel>
                 <%--Fim Menu Editar Perfil--%>
             </div>
         </div>
--- code/TasteAdvisor/TasteAdvisor/ClientProfile.aspx.cs
+++ code/TasteAdvisor/TasteAdvisor/ClientProfile.aspx.cs
@@ -11,7 +11,17 @@ namespace TasteAdvisor
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (!IsPostBack)
+            {
+                restaurantesFavoritosPanel.Visible = false;
+                pratosFavoritosPanel.Visible = false;
+                restaurantesNaoFavoritosPanel.Visible = false;
+                pratosNaoFavoritosPanel.Visible = false;
+                outrasPreferenciasPanel.Visible = true;
+                meusComentariosPanel.Visible = false;
+                minhasFotografiasPanel.Visible = false;
+                editarPerfilMainPanel.Visible = false;
+            }
             
         }
 
@@ -23,6 +33,7 @@ namespace TasteAdvisor
             {
                 TreeNode parent = new TreeNode();
                 parent.Text = "Minhas Preferencias";
+
                 TreeNode child = new TreeNode();
                     child.Text = "Favoritos";
                     parent.ChildNodes.Add(child);
@@ -34,7 +45,7 @@ namespace TasteAdvisor
                         child.ChildNodes.Add(childChild);
                     child = new TreeNode();
                     child.Text = "Não Favoritos";
-                    parent.ChildNodes.Add(child);
+                parent.ChildNodes.Add(child);
                         childChild = new TreeNode();
                         childChild.Text = "Restaurantes";
                         child.ChildNodes.Add(childChild);
@@ -43,7 +54,7 @@ namespace TasteAdvisor
                         child.ChildNodes.Add(childChild);
                     child = new TreeNode();
                     child.Text = "Outras Preferências";
-                    parent.ChildNodes.Add(child);
+                parent.ChildNodes.Add(child);
                 
                 menuTreeView.Nodes.Add(parent);
                 parent = new TreeNode();
@@ -55,6 +66,7 @@ namespace TasteAdvisor
                 parent = new TreeNode();
                 parent.Text = "Editar Perfil";
                 menuTreeView.Nodes.Add(parent);
+
             }
             TreeNode node = new TreeNode();
             menuTreeView.FindNode("Minhas Preferências");
@@ -73,5 +85,130 @@ namespace TasteAdvisor
             }
         }
         #endregion
+
+        protected void menuTreeView_SelectedNodeChanged(object sender, EventArgs e)
+        {
+            if ((sender as TreeView).SelectedNode.Text.Trim() == "Minhas Preferências")
+            {
+                restaurantesFavoritosPanel.Visible = false;
+                pratosFavoritosPanel.Visible = false;
+                restaurantesNaoFavoritosPanel.Visible = false;
+                pratosNaoFavoritosPanel.Visible = false;
+                outrasPreferenciasPanel.Visible = true;
+                meusComentariosPanel.Visible = false;
+                minhasFotografiasPanel.Visible = false;
+                editarPerfilMainPanel.Visible = false;
+            }
+            else if ((sender as TreeView).SelectedNode.Text.Trim() == "Favoritos")
+            {
+                restaurantesFavoritosPanel.Visible = true;
+                pratosFavoritosPanel.Visible = false;
+                restaurantesNaoFavoritosPanel.Visible = false;
+                pratosNaoFavoritosPanel.Visible = false;
+                outrasPreferenciasPanel.Visible = false;
+                meusComentariosPanel.Visible = false;
+                minhasFotografiasPanel.Visible = false;
+                editarPerfilMainPanel.Visible = false;
+            }
+            else if ((sender as TreeView).SelectedNode.Text.Trim() == "Restaurantes" && (sender as TreeView).SelectedNode.ValuePath.Contains("Não Favoritos"))
+            {
+                restaurantesFavoritosPanel.Visible = false;
+                pratosFavoritosPanel.Visible = false;
+                restaurantesNaoFavoritosPanel.Visible = true;
+                pratosNaoFavoritosPanel.Visible = false;
+                outrasPreferenciasPanel.Visible = false;
+                meusComentariosPanel.Visible = false;
+                minhasFotografiasPanel.Visible = false;
+                editarPerfilMainPanel.Visible = false;
+            }
+            else if ((sender as TreeView).SelectedNode.Text.Trim() == "Pratos" && (sender as TreeView).SelectedNode.ValuePath.Contains("Não Favoritos"))
+            {
+                restaurantesFavoritosPanel.Visible = false;
+                pratosFavoritosPanel.Visible = false;
+                restaurantesNaoFavoritosPanel.Visible = false;
+                pratosNaoFavoritosPanel.Visible = true;
+                outrasPreferenciasPanel.Visible = false;
+                meusComentariosPanel.Visible = false;
+                minhasFotografiasPanel.Visible = false;
+                editarPerfilMainPanel.Visible = false;
+            }
+            else if ((sender as TreeView).SelectedNode.Text.Trim() == "Não Favoritos")
+            {
+                restaurantesFavoritosPanel.Visible = false;
+                pratosFavoritosPanel.Visible = false;
+                restaurantesNaoFavoritosPanel.Visible = true;
+                pratosNaoFavoritosPanel.Visible = false;
+                outrasPreferenciasPanel.Visible = false;
+                meusComentariosPanel.Visible = false;
+                minhasFotografiasPanel.Visible = false;
+                editarPerfilMainPanel.Visible = false;
+            }
+            else if ((sender as TreeView).SelectedNode.Text.Trim() == "Restaurantes" && (sender as TreeView).SelectedNode.ValuePath.Contains("Favoritos"))
+            {
+                restaurantesFavoritosPanel.Visible = true;
+                pratosFavoritosPanel.Visible = false;
+                restaurantesNaoFavoritosPanel.Visible = false;
+                pratosNaoFavoritosPanel.Visible = false;
+                outrasPreferenciasPanel.Visible = false;
+                meusComentariosPanel.Visible = false;
+                minhasFotografiasPanel.Visible = false;
+                editarPerfilMainPanel.Visible = false;
+            }
+            else if ((sender as TreeView).SelectedNode.Text.Trim() == "Pratos" && (sender as TreeView).SelectedNode.ValuePath.Contains("Favoritos"))
+            {
+                restaurantesFavoritosPanel.Visible = false;
+                pratosFavoritosPanel.Visible = true;
+                restaurantesNaoFavoritosPanel.Visible = false;
+                pratosNaoFavoritosPanel.Visible = false;
+                outrasPreferenciasPanel.Visible = false;
+                meusComentariosPanel.Visible = false;
+                minhasFotografiasPanel.Visible = false;
+                editarPerfilMainPanel.Visible = false;
+            }
+            else if ((sender as TreeView).SelectedNode.Text.Trim() == "Outras Preferências")
+            {
+                restaurantesFavoritosPanel.Visible = false;
+                pratosFavoritosPanel.Visible = false;
+                restaurantesNaoFavoritosPanel.Visible = false;
+                pratosNaoFavoritosPanel.Visible = false;
+                outrasPreferenciasPanel.Visible = true;
+                meusComentariosPanel.Visible = false;
+                minhasFotografiasPanel.Visible = false;
+                editarPerfilMainPanel.Visible = false;
+            }
+            else if ((sender as TreeView).SelectedNode.Text.Trim() == "Meus Comentários")
+            {
+                restaurantesFavoritosPanel.Visible = false;
+                pratosFavoritosPanel.Visible = false;
+                restaurantesNaoFavoritosPanel.Visible = false;
+                pratosNaoFavoritosPanel.Visible = false;
+                outrasPreferenciasPanel.Visible = false;
+                meusComentariosPanel.Visible = true;
+                minhasFotografiasPanel.Visible = false;
+                editarPerfilMainPanel.Visible = false;
+            }
+            else if ((sender as TreeView).SelectedNode.Text.Trim() == "Minhas Fotografias")
+            {
+                restaurantesFavoritosPanel.Visible = false;
+                pratosFavoritosPanel.Visible = false;
+                restaurantesNaoFavoritosPanel.Visible = false;
+                pratosNaoFavoritosPanel.Visible = false;
+                outrasPreferenciasPanel.Visible = false;
+                meusComentariosPanel.Visible = false;
+                minhasFotografiasPanel.Visible = true;
+                editarPerfilMainPanel.Visible = false;
+            }
+            else if ((sender as TreeView).SelectedNode.Text.Trim() == "Editar Perfil")
+            {
+                restaurantesFavoritosPanel.Visible = false;
+                pratosFavoritosPanel.Visible = false;
+                restaurantesNaoFavoritosPanel.Visible = false;
+                pratosNaoFavoritosPanel.Visible = false;
+                outrasPreferenciasPanel.Visible = false;
+                meusComentariosPanel.Visible = false;
+                minhasFotografiasPanel.Visible = false;
+                editarPerfilMainPanel.Visible = true;
+            }
+        }
     }
 }
